5ee6c6f84fd98a7fa2e9f391355cef19baa9d978
rupnikk <rupnik.urban@gmail.com>
2016-10-21 07:30:42 +0200

    stiling

--- zajemanje.py
+++ zajemanje.py
@@ -8,6 +8,7 @@ Created on Sun Oct 16 20:45:07 2016
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook,load_workbook
+from openpyxl.styles import Font, Border, Alignment, Side
 import os
 
 
@@ -19,16 +20,84 @@ mesec=['Januar','Februar','Marec','April',
 'Maj', 'Junij', 'Julij', 'Avgust', 
 'September', 'Oktober', 'November', 'December']
 
+ft=Font(bold=True)
+bd1=Border(bottom=Side(border_style='thick', color='FF000000'))
+al1=Alignment(horizontal='center', vertical='center')
+
 if (os.path.isfile(filename)):
 	wb=load_workbook(filename)
 	for i in range(0,12):
 		ws[i]=wb.get_sheet_by_name(mesec[i])
+
 else:
 	wb=Workbook()
-	ws[0]=wb.active
-	ws[0].title='Januar'
-	for i in range(1,12):
-		ws[i]=wb.create_sheet(mesec[i])
+	for i in range(0,12):
+		if (i==0):
+			ws[0]=wb.active
+			ws[0].title='Januar'
+		else:
+			ws[i]=wb.create_sheet(mesec[i])
+
+		a1=ws[i]["A1"]
+		b1=ws[i]["B1"]
+		c1=ws[i]["C1"]
+		d1=ws[i]["D1"]
+		e1=ws[i]["E1"]
+		f1=ws[i]["F1"]
+
+		a1.value="Dan"
+		b1.value="Datum in ura"
+		c1.value="Temperatura"
+		d1.value="Vlažnost"
+		e1.value="Padavine"
+		f1.value="Padavine 12h"
+
+		col1=ws[i].column_dimensions['A']
+		col2=ws[i].column_dimensions['B']
+		col3=ws[i].column_dimensions['C']
+		col4=ws[i].column_dimensions['D']
+		col5=ws[i].column_dimensions['E']
+		col6=ws[i].column_dimensions['F']
+
+		a1.font=ft
+		b1.font=ft
+		c1.font=ft
+		d1.font=ft
+		e1.font=ft
+		f1.font=ft
+
+		a1.border=bd1
+		b1.border=bd1
+		c1.border=bd1
+		d1.border=bd1
+		e1.border=bd1
+		f1.border=bd1
+
+		col1.alignment=al1
+		col2.alignment=al1
+		col3.alignment=al1
+		col4.alignment=al1
+		col5.alignment=al1
+		col6.alignment=al1
+
+		a1.alignment=al1
+		b1.alignment=al1
+		c1.alignment=al1
+		d1.alignment=al1
+		e1.alignment=al1
+		f1.alignment=al1
+
+		col1.width = 10
+		col2.width = 18
+		col3.width = 14
+		col4.width = 10
+		col5.width = 10
+		col6.width = 14
+
+
+
+
+
 wb.save(filename)
 
 url = "http://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observationAms_ZADLOG_history.html"
@@ -80,7 +149,7 @@ for index1, tr in enumerate(table.find_all("tr")[1:]):  # skip first row
     if(date+ ' ' + time1 == last_old_value):
 		break
 
-    data.append((date+ ' ' + time1, day, temperature_precise, temparature, humidity_precise, rainfall, rainfall_12h))
+    data.append((date+ ' ' + time1, day, temperature_precise, humidity_precise, rainfall, rainfall_12h))
 
 data.reverse();
 
@@ -95,7 +164,6 @@ for index, sample in enumerate(data):
 			dotidx2=x.find('.', dotidx1+1)
 			month = int(x[dotidx1+1:dotidx2])-1
 			ws[month]['B'+str(index+j+1)]=x
-			ws[month].column_dimensions["B"].width = 15
 		elif(idx == 1):
 			ws[month]['A'+str(index+j+1)]=x
 		elif(idx==2):
@@ -106,8 +174,8 @@ for index, sample in enumerate(data):
 			ws[month]['E'+str(index+j+1)]=float(x)
 		elif(idx==5):
 			ws[month]['F'+str(index+j+1)]=float(x)
-		elif(idx==6):
-			ws[month]['G'+str(index+j+1)]=float(x)
+#		elif(idx==6):
+#			ws[month]['G'+str(index+j+1)]=float(x)
 #		elif(idx==7):
 #			ws[month]['H'+str(index+2)]=x
 #print(data)
